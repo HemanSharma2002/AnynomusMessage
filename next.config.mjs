c4bbca390ae63e4184978226748d42c4e91dd2a1
Heman Sharma <hemansharma2002@gmail.com>
2024-05-18 17:47:35 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,8 @@
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    // Other config options...
+    pageBuildTimeout: 120000, // Set to a higher value (e.g., 2 minutes)
+  };
+  
